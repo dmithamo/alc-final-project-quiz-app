47f496c17f421857ec0b75bcf0b7e6811d7f66ce
dmithamo <dennisbmithamo@gmail.com>
2018-03-02 01:06:28 +0300

    Refactor, to facilitate DRY code.

--- app/src/main/java/com/example/dennis/quizapp/MainActivity.java
+++ app/src/main/java/com/example/dennis/quizapp/MainActivity.java
@@ -14,23 +14,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * Method (it and the one below it) compare selected responses to
+     * correct ones and update score as applies
+     * Also update Grade and Remark
+     *
+     * @param view
+     */
     public void gradeQuiz(View view) {
+
+//      Question One params
+        RadioGroup questionOne = (RadioGroup) findViewById(R.id.rg_qn1);
+        int selectedQnOneID = questionOne.getCheckedRadioButtonId();
+        int correctQnOneID = findViewById(R.id.soln_1b).getId();
+
+//      Call method that validates answers with qn one params
+        checkAnswers(selectedQnOneID, correctQnOneID);
+
+
+    }
+
+    public void checkAnswers(int correctID, int selectedID) {
         int score = 0;
         int grade = 0;
         String remark = "";
 
-
-        RadioGroup questionOne = (RadioGroup) findViewById(R.id.rg_qn1);
-        int correctQnOneID = findViewById(R.id.soln_1b).getId();
-
-        int selectedQnOneID = questionOne.getCheckedRadioButtonId();
-        if (selectedQnOneID == correctQnOneID) {
+        if (selectedID == correctID) {
             score += 1;
         }
 
-        String displayScore = getString(R.string.your_score) + " " + score;
-        TextView displayScoreText = (TextView) findViewById(R.id.your_score);
-        displayScoreText.setText(displayScore);
+        String displayScore = getString(R.string.your_score) + " " + score + " out of 15.";
+        TextView scoreTextView = (TextView) findViewById(R.id.your_score);
+        scoreTextView.setText(displayScore);
 
     }
 }
