939d2ecd469348b277b85f39fd52ee7f35493287
dmithamo <dennisbmithamo@gmail.com>
2018-03-02 00:53:39 +0300

    Test GradeQuiz button (successfully :) )

--- app/src/main/java/com/example/dennis/quizapp/MainActivity.java
+++ app/src/main/java/com/example/dennis/quizapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.dennis.quizapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void gradeQuiz(View view) {
+        int score = 0;
+        int grade = 0;
+        String remark = "";
+
+
+        RadioGroup questionOne = (RadioGroup) findViewById(R.id.rg_qn1);
+        int correctQnOneID = findViewById(R.id.soln_1b).getId();
+
+        int selectedQnOneID = questionOne.getCheckedRadioButtonId();
+        if (selectedQnOneID == correctQnOneID) {
+            score += 1;
+        }
+
+        String displayScore = getString(R.string.your_score) + " " + score;
+        TextView displayScoreText = (TextView) findViewById(R.id.your_score);
+        displayScoreText.setText(displayScore);
 
     }
 }
